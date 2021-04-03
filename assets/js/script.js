d6e2a3ece0ac3a7272694df2ea6404242fdb820d
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-03 10:05:48 -0500

    displays list but not of city searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,23 @@ function weatherAPI() {
     var searchTerm = document.querySelector("#searchTerm").value;
     console.log(searchTerm);
     fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=663fbbeadd446c56388d07f33b9c6bca`
+        `https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=663fbbeadd446c56388d07f33b9c6bca`
     )
-    .then(funtion() {
-        return 
+    .then(function(weatherResponse) {
+        return weatherResponse.json();   
+    })
+    .then(function(weatherResponse) {
+        var displayCity = document.querySelector("#list-city-search");
+
+        var listCitySearchEl = document.createElement("li");
+        listCitySearchEl.className="city-list";
+        listCitySearchEl.innerHTML= "<h3 class='city-name'>" + searchTerm.value + "</h3>"
+        displayCity.appendChild(listCitySearchEl);
+
+        
+        // var weatherLat = weatherResponse.lat;
+
     })
 }
+
+weatherAPI();
