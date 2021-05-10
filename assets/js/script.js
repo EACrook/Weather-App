aae46489d14e3290a84aa2e835f5a1b243db8274
Emily Crook <Emily.A.Crook96@gmail.com>
2021-05-10 10:55:53 -0500

    searched city is put into API and the API returns information about city searched in network settings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,30 @@
 function weatherAPI() {
-    var searchTerm = document.querySelector("#searchTerm").value;
-    console.log(searchTerm);
+    const city = document.getElementById('searchTerm').value
+
     fetch(
-        `https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=663fbbeadd446c56388d07f33b9c6bca`
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=663fbbeadd446c56388d07f33b9c6bca`
     )
-    .then(function(weatherResponse) {
-        return weatherResponse.json();   
-    })
-    .then(function(weatherResponse) {
-        var displayCity = document.querySelector("#list-city-search");
+        .then(function(locationResponse) {
+            return locationResponse.json();
+        })
+    // var searchTerm = document.querySelector("#searchTerm").value;
+    // console.log(searchTerm);
+    // fetch(
+    //     `https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&appid=663fbbeadd446c56388d07f33b9c6bca`
+    // )
+    // .then(function(weatherResponse) {
+    //     return weatherResponse.json();   
+    // })
+    // .then(function(weatherResponse) {
+    //     var displayCity = document.querySelector("#list-city-search");
 
-        var listCitySearchEl = document.createElement("li");
-        listCitySearchEl.className="city-list";
-        listCitySearchEl.innerHTML= "<h3 class='city-name'>" + searchTerm.value + "</h3>"
-        displayCity.appendChild(listCitySearchEl);
+    //     var listCitySearchEl = document.createElement("li");
+    //     listCitySearchEl.className="city-list";
+    //     listCitySearchEl.innerHTML= "<h3 class='city-name'>" + searchTerm.value + "</h3>"
+    //     displayCity.appendChild(listCitySearchEl);
+    //     // var weatherLat = weatherResponse.lat;
 
-        
-        // var weatherLat = weatherResponse.lat;
-
-    })
+    // })
 }
 
 weatherAPI();
