37ed3f1872c05f0535830c36d890614690ccc4ab
Emily Crook <Emily.A.Crook96@gmail.com>
2021-05-27 18:30:35 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Weather-App
+
+## Introduction
+The Weather App pulls data through a 3rd party API to portray the weather for the current day as well as a 5 day weather report for a searched city. When a city is searched, it will be saved to the local server to portray the recent searches. 
+
+## Screenshots
+
+## Deployed Page
+https://eacrook.github.io/Weather-App/
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@
 
 function weatherAPI() {
     var city = document.getElementById('searchTerm').value
+    displayCitySearchTerm(city)
+
+    var cities = [];
+
+    if(  localStorage.getItem('cities')) {
+        cities = JSON.parse( localStorage.getItem('cities'))
+
+    }
+
+    cities.push(city)
+    localStorage.setItem('cities', JSON.stringify(cities))
 
     fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=663fbbeadd446c56388d07f33b9c6bca`
@@ -30,35 +41,91 @@ function weatherAPI() {
             var temperature = response.current.temp;
 
             var windSpeed = response.current.wind_speed;
-            
+
             var humidity = response.current.humidity;
 
             var uvIndex = response.current.uvi;
 
-            var displayCity = document.querySelector("#list-city-search");
+            
 
             var displayCityInfo = document.querySelector("#one-day-forecast");
+            displayCityInfo.innerHTML = ""
 
-            var listCitySearchEl = document.createElement("li");
-            listCitySearchEl.className = "city-list";
-            listCitySearchEl.innerHTML = "<div class='city-name'>" + searchTerm.value + "</div>"
-            displayCity.appendChild(listCitySearchEl);
+          
 
             var cityNameEl = document.createElement("h2");
-            cityNameEl.innerHTML =`<h2>${city}</h2>`;
+            cityNameEl.innerHTML = `<h2>${city}</h2>`;
             displayCityInfo.appendChild(cityNameEl)
 
             var cityInfoEl = document.createElement("div");
-            cityInfoEl.innerHTML = 
-            `<div>
+            cityInfoEl.innerHTML =
+                `<div>
             Temp: ${temperature} 
             <br>Wind: ${windSpeed} 
             <br>Humidity: ${humidity} 
             <br>UV Index: ${uvIndex} 
             </div>`;
             displayCityInfo.appendChild(cityInfoEl);
+
+            fiveDay(response.daily)
         })
- 
+    // .then(function (response) {
+    //   console.log(response.daily)
+    //})
+
 }
+
+function displayCitySearchTerm(searchTerm) {
+    var displayCity = document.querySelector("#list-city-search");
+    var listCitySearchEl = document.createElement("li");
+    listCitySearchEl.className = "city-list";
+    listCitySearchEl.innerHTML = "<div class='city-name'>" + searchTerm + "</div>"
+    displayCity.appendChild(listCitySearchEl);
+}
+
+function displayPastSeaches() {
+    var pastSearches = JSON.parse(localStorage.getItem('cities'))
+    for (let i = 0; i < pastSearches.length; i++) {
+        displayCitySearchTerm(pastSearches[i])
+        
+    }
+}
+displayPastSeaches()
+function fiveDay(daily) {
+    console.log('Dailys function!!!', daily[0])
+    var fiveDayDisplay = document.querySelector("#five-day-forecast")
+    fiveDayDisplay.innerHTML = ""
+    
+    for (let i = 1; i < 6; i++) {
+
+        var date = daily[i].dt
+
+        var weatherPicture = "http://openweathermap.org/img/w/" + daily[i].weather[0].icon + ".png" 
+
+        var dailytemp = daily[i].temp.day
+
+        var dailyWind = daily[i].wind_speed
+
+        var dailyHumidity = daily[i].humidity
+
+    
+
+        var cityInfoEl = document.createElement("div");
+        cityInfoEl.innerHTML =
+            `<div>
+    Date: ${date}
+    <br> <img src=${weatherPicture}></img>
+    <br>Temp: ${dailytemp} 
+    <br>Wind: ${dailyWind} 
+    <br>Humidity: ${dailyHumidity} 
+    </div>`;
+        fiveDayDisplay.appendChild(cityInfoEl);
+    }
+
+}
+
+// function singleDay() {
+
+// }
 
 weatherAPI();
